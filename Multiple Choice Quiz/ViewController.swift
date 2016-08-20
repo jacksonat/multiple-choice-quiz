f622991f1ce7a578a3cdda2d943cddca9674e0b3
Jackson Taylor <closprieur@gmail.com>
2016-08-20 14:11:56 +1000

    Initial Commit

---
+++ Multiple Choice Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiple Choice Quiz
+//
+//  Created by Jackson Taylor on 20/08/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
