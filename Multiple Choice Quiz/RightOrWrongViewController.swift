08d922dc0dce66cf6878752dc295491235ba87d6
Jackson Taylor <closprieur@gmail.com>
2016-08-20 14:51:14 +1000

    Add four view controllers for different quizzes

---
+++ Multiple Choice Quiz/RightOrWrongViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RightOrWrongViewController.swift
+//  Multiple Choice Quiz
+//
+//  Created by Jackson Taylor on 20/08/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import UIKit
+
+class RightOrWrongViewController: NSObject {
+
+}
