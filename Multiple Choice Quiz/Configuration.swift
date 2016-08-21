f3cab431be8d9d40bbca07a031baf05e096ebb0c
Jackson Taylor <closprieur@gmail.com>
2016-08-21 19:16:55 +1000

    Create configuration file for the multiple choice dictionary array

---
+++ Multiple Choice Quiz/Configuration.swift
@@ -0,0 +1,16 @@
+//
+//  Configuration.swift
+//  Multiple Choice Quiz
+//
+//  Created by Jackson Taylor on 21/08/2016.
+//  Copyright © 2016 Jackson Taylor. All rights reserved.
+//
+
+import Foundation
+
+var helloWorld = "Hello World"
+
+// create an array of dictionaries, with strings as keys and any object as values
+var multipleChoiceArray: Array<[String: AnyObject]>?
+
+
--- Multiple Choice Quiz/ViewController.swift
+++ Multiple Choice Quiz/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
