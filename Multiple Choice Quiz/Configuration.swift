7552ebf1ebac9e889a22d2d2c0006799756f7ff0
Jackson Taylor <closprieur@gmail.com>
2016-08-22 21:01:02 +1000

    Replace plist questions with a struct of questions in Configuration file

--- Multiple Choice Quiz/Configuration.swift
+++ Multiple Choice Quiz/Configuration.swift
@@ -11,6 +11,31 @@ import Foundation
 var helloWorld = "Hello World"
 
 // create an array of dictionaries, with strings as keys and any object as values
-var multipleChoiceArray: Array<[String: AnyObject]>?
+
+// Old code using plist commented out
+//var multipleChoiceArray: Array<[String: AnyObject]>?
+
+// Hard code the questions list here as a struct
+
+struct Question {
+    
+    var Question: String?
+    var Answers: [String]?
+    var Answer: String // Could also be Int if the correct check is to the button array index
+    
+}
+
+var QuestionsList = [
+    
+    Question(Question: "What is the capital of the U.K?", Answers: ["London", "Berlin", "New York", "Heathrow"], Answer: "London"),
+    Question(Question: "What is the capital of the United States?", Answers: ["New York", "Los Angeles", "Seattle", "Washington D.C."], Answer: "Washinton D.C."),
+    Question(Question: "What is the capital of France?", Answers: ["Paris", "Nice", "Lyon", "Marseilles"], Answer: "Paris"),
+    Question(Question: "What is the capital of Germany", Answers: ["Frankfurt", "Berlin", "Bonn", "Strasburg"], Answer: "")
+
+]
+
+
+
+
 
 
--- Multiple Choice Quiz/ViewController.swift
+++ Multiple Choice Quiz/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        loadQuizData()
+    
     }
     
     // Remove navigation bar from root view controller
@@ -29,7 +32,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadQuizData() {
     
+       /*
+        
+        // Multiple Choice Data
+        let pathMC = NSBundle.mainBundle().pathForResource("MCQuestions", ofType: "plist")
+        let dictMC = NSDictionary(contentsOfFile: pathMC!)
+        
+        // This stores the questions in multipleChoiceArray. Could just hardcode the array in the config file if want to avoid plist. Downsides?
+        // This line below failed at run time. Reverting to not using plist but putting questions in linked config file
+        // multipleChoiceArray = dictMC!["Questions"]!.mutableCopy as! Array
+    
+        check()
+ 
+        */
+        
+        check()
+ 
+    }
+ 
+    
+    func check(){
+    
+        print(QuestionsList)
+    
+    }
 
 
 }
